ab83410fd41460bf6871387bf59a02006bfcefff
Vandan-Bhangale <vandan9804@gmail.com>
2025-09-09 10:00:27 +0530

    Updated code

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const mongodbStore = require('connect-mongodb-session')(session);
 const userRoutes = require('./Routes/userRoute');
 const carRoutes = require('./Routes/CarRoutes');
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(cors({origin: process.env.CORS_ORIGIN, credentials:true}));
 app.use(express.json());
+app.use(cookieParser());
 const URI = process.env.MONGODB_URI;
 
 const store = new mongodbStore({
@@ -20,19 +22,33 @@ const store = new mongodbStore({
 });
 
 app.use('/uploads', express.static('uploads'));
-
+app.set("trust proxy", 1);  // trust first proxy (Render/Heroku/etc.)
 
 //Session for localhost
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     store: store,
+//     cookie: {
+//         httpOnly: true,
+//         secure:false,
+//         maxAge: 1000 * 60 * 60 * 24 // 1 day
+//     }
+// }));
+
+// This session is for deployed version on render and vercel
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-    cookie: {
-        httpOnly: true,
-        secure:false,
-        maxAge: 1000 * 60 * 60 * 24 // 1 day
-    }
+  secret: process.env.SESSION_SECRET,  
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: true,       
+    sameSite: "none",  
+    maxAge: 24 * 60 * 60 * 1000 
+  },
+  store
 }));
 
 app.use('/api',userRoutes)
